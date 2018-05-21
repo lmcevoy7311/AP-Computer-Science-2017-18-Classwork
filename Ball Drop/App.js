134a9f7fc589fd63eda57d4b6346d10e48107e48
lmcevoy7311 <39276095+lmcevoy7311@users.noreply.github.com>
2018-05-21 13:51:46 -0400

    Add files via upload

---
+++ Ball Drop/App.js
@@ -0,0 +1,237 @@
+import React, { Component } from 'react';
+import { View, WebView, StatusBar } from 'react-native';
+
+export default class App extends Component {
+    render() {
+
+        var webViewCode = `
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://static.codehs.com/gulp/4e6f13f254c3349da3545c0301b74b1806afea50/chs-js-lib/chs.js"></script>
+
+<style>
+    body, html {
+        margin: 0;
+        padding: 0;
+    }
+    canvas {
+        margin: 0px;
+        padding: 0px;
+        display: inline-block;
+        vertical-align: top;
+    }
+    #btn-container {
+        text-align: center;
+        padding-top: 10px;
+    }
+    #btn-play {
+        background-color: #8cc63e;
+    }
+    #btn-stop {
+        background-color: #de5844;
+    }
+    .glyphicon {
+        margin-top: -3px;
+        color: #FFFFFF;
+    }
+</style>
+</head>
+
+<body>
+    <div id="canvas-container" style="margin: 0 auto; ">
+        <canvas
+        id="game"
+        width="400"
+        height="480"
+        class="codehs-editor-canvas"
+        style="width: 100%; height: 100%; margin: 0 auto;"
+        ></canvas>
+    </div>
+    <div id="console"></div>
+    <div id="btn-container">
+        <button class="btn btn-main btn-lg" id="btn-play" onclick='stopProgram(); runProgram();'><span class="glyphicon glyphicon-play" aria-hidden="true"></span></button>
+        <button class="btn btn-main btn-lg" id="btn-stop" onclick='stopProgram();'><span class="glyphicon glyphicon-stop" aria-hidden="true"></span></button>
+    </div>
+
+<script>
+    var console = {};
+    console.log = function(msg){
+        $("#console").html($("#console").html() + "     " + msg);
+    };
+
+    var runProgram = function() {
+        var sun = new Circle(25); sun.setColor(Color.ORANGE); sun.setPosition(getWidth() / 2, getHeight() / 2); add(sun);
+var grid = new Grid(100, 4);
+for(var i = 0; i < grid.numRows(); i++)
+{
+    grid.set(i, 0, new Circle(10));
+    grid.set(i, 1, false);
+
+}
+//var line = new Line(sun.getX(), sun.getY(), sun.getX(), sun.getY() + 10); add(line);
+
+var time = 0;
+var x = getWidth()/2;
+var y = getHeight()/2;
+
+start();
+function start()
+{
+    mouseClickMethod(klick);
+    setTimer(update, 10);
+}
+
+function klick(e)
+{
+    var t = false;
+    var planet;
+    var dist;
+    for(var i = 0; i < grid.numRows(); i++)
+    {
+        if(grid.get(i, 1) == false)
+        {
+            t = true;
+            grid.set(i, 1, true);
+            grid.get(i, 0).setPosition(e.getX(), e.getY()); grid.get(i, 0).setColor(Randomizer.nextColor()); add(grid.get(i, 0));
+            planet = grid.get(i, 0);
+            planet.setRadius(Randomizer.nextInt(5, 15));
+            add(grid.get(i,0));
+            console.log(grid.get(i, 4) + ", " + grid.get(i, 4));
+            i = grid.numRows() +1;
+        }
+    }
+    if(t == false)
+    {
+        console.log("You Suck!");
+    }
+}
+
+
+
+function update()
+{
+    add(sun);
+    for(var i = 0; i < grid.numRows(); i++)
+    {
+        if(grid.get(i, 1) == true)
+        {
+            grid.get(i,0).move(0,1);
+        }
+    }
+}
+
+
+        if (typeof start === 'function') {
+            start();
+        }
+
+        // Overrides setSize() if called from the user's code. Needed because
+        // we have to change the canvas size and attributes to reflect the
+        // user's desired program size. Calling setSize() from user code only
+        // has an effect if Fit to Full Screen is Off. If Fit to Full Screen is
+        // On, then setSize() does nothing.
+        function setSize(width, height) {
+            if (!true) {
+                // Call original graphics setSize()
+                window.__graphics__.setSize(width, height);
+
+                // Scale to screen width but keep aspect ratio of program
+                // Subtract 2 to allow for border
+                var canvasWidth = window.innerWidth - 2;
+                var canvasHeight = canvasWidth * getHeight() / getWidth();
+
+                // Make canvas reflect desired size set
+                adjustMarginTop(canvasHeight);
+                setCanvasContainerSize(canvasWidth, canvasHeight);
+                setCanvasAttributes(canvasWidth, canvasHeight);
+            }
+        }
+    };
+
+    var stopProgram = function() {
+        removeAll();
+        window.__graphics__.fullReset();
+    }
+
+    window.onload = function() {
+        if (!false) {
+            $('#btn-container').remove();
+        }
+
+        var canvasWidth;
+        var canvasHeight;
+        if (true) {
+            // Get device window width and set program size to those dimensions
+            setSize(window.innerWidth, window.innerHeight);
+            canvasWidth = getWidth();
+            canvasHeight = getHeight();
+
+            if (false) {
+                // Make room for buttons if being shown
+                $('#btn-container').css('padding', '5px 0');
+                canvasHeight -= $('#btn-container').outerHeight();
+            }
+
+            setCanvasAttributes(canvasWidth, canvasHeight);
+        } else {
+            // Scale to screen width but keep aspect ratio of program
+            // Subtract 2 to allow for border
+            canvasWidth = window.innerWidth - 2;
+            canvasHeight = canvasWidth * getHeight() / getWidth();
+
+            // Light border around canvas if not full screen
+            $('#canvas-container').css('border', '1px solid #beccd4');
+
+            adjustMarginTop(canvasHeight);
+        }
+
+        setCanvasContainerSize(canvasWidth, canvasHeight);
+
+        if (true) {
+            runProgram();
+        }
+    };
+
+    // Set the canvas container width and height.
+    function setCanvasContainerSize(width, height) {
+        $('#canvas-container').width(width);
+        $('#canvas-container').height(height);
+    }
+
+    // Set the width and height attributes of the canvas. Allows
+    // getTouchCoordinates to sense x and y correctly.
+    function setCanvasAttributes(canvasWidth, canvasHeight) {
+        $('#game').attr('width', canvasWidth);
+        $('#game').attr('height', canvasHeight);
+    }
+
+    // Assumes the Fit to Full Screen setting is Off. Adjusts the top margin
+    // depending on the Show Play/Stop Buttons setting.
+    function adjustMarginTop(canvasHeight) {
+        var marginTop = (window.innerHeight - canvasHeight)/2;
+        if (false) {
+            marginTop -= $('#btn-container').height()/3;
+        }
+        $('#canvas-container').css('margin-top', marginTop);
+    }
+</script>
+</body>
+</html>
+`;
+        return (
+            <View style={{ flex: 1 }}>
+                <StatusBar hidden />
+                <WebView
+                    source={{html: webViewCode, baseUrl: "/"}}
+                    javaScriptEnabled={true}
+                    style={{ flex: 1 }}
+                    scrollEnabled={false}
+                    bounces={false}
+                    scalesPageToFit={false}
+                ></WebView>
+            </View>
+        );
+    }
+}
